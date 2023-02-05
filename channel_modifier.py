2f790d0c4e50bfeb3887d9f6e338bcfcff8cff8b
CupKido <taler@g.jct.ac.il>
2023-02-05 23:42:18 +0200

    added specific role support

--- channel_modifier.py
+++ channel_modifier.py
@@ -1,9 +1,13 @@
 import discord
 
-async def set_readonly(channel):
+async def set_readonly(channel, role = None):
+    if role == None:
+        role = channel.guild.default_role
     await channel.edit(sync_permissions=True)
     await channel.set_permissions(channel.guild.default_role, send_messages=False, read_messages=True, create_public_threads=False, create_private_threads=False)
 
-async def set_writable(channel):
+async def set_writable(channel, role = None):
+    if role == None:
+        role = channel.guild.default_role
     await channel.edit(sync_permissions=True)
     await channel.set_permissions(channel.guild.default_role, send_messages=True, read_messages=True, create_public_threads=True, create_private_threads=True)
