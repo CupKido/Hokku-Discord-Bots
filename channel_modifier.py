698640e92b97c58755d8886b19d00d6ae91e1111
CupKido <taler@g.jct.ac.il>
2023-02-06 00:04:59 +0200

    improved channel modifier

--- channel_modifier.py
+++ channel_modifier.py
@@ -6,8 +6,8 @@ async def set_readonly(channel, role = None):
     await channel.edit(sync_permissions=True)
     await channel.set_permissions(channel.guild.default_role, send_messages=False, read_messages=True, create_public_threads=False, create_private_threads=False)
 
-async def set_writable(channel, role = None):
+async def remove_readonly(channel, role = None):
     if role == None:
         role = channel.guild.default_role
     await channel.edit(sync_permissions=True)
-    await channel.set_permissions(channel.guild.default_role, send_messages=True, read_messages=True, create_public_threads=True, create_private_threads=True)
+    await channel.set_permissions(channel.guild.default_role, send_messages=None, read_messages=None, create_public_threads=None, create_private_threads=None)
