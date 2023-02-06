05f832d75b25de50c26a72331bfb6f30d027ea66
CupKido <taler@g.jct.ac.il>
2023-02-06 18:54:14 +0200

    fixed channels modifier

--- channel_modifier.py
+++ channel_modifier.py
@@ -6,7 +6,7 @@ async def set_readonly(channel, role = None):
     if role == None:
         role = channel.guild.default_role
     await channel.edit(sync_permissions=True)
-    await channel.set_permissions(channel.guild.default_role, send_messages=False, read_messages=True, create_public_threads=False, create_private_threads=False)
+    await channel.set_permissions(role, send_messages=False, read_messages=True, create_public_threads=False, create_private_threads=False)
 
 async def remove_readonly(channel, role = None):
     if channel is None:
@@ -14,4 +14,12 @@ async def remove_readonly(channel, role = None):
     if role == None:
         role = channel.guild.default_role
     await channel.edit(sync_permissions=True)
-    await channel.set_permissions(channel.guild.default_role, send_messages=None, read_messages=None, create_public_threads=None, create_private_threads=None)
+    await channel.set_permissions(role, send_messages=None, read_messages=None, create_public_threads=None, create_private_threads=None)
+
+async def give_management(channel, role = None):
+    if channel is None:
+        return
+    if role == None:
+        role = channel.guild.default_role
+    await channel.edit(sync_permissions=True)
+    await channel.set_permissions(role, manage_channels=True)
