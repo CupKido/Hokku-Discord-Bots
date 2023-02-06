1c1aaecb4582762c3b8fba9566c1a1a711f343ec
CupKido <taler@g.jct.ac.il>
2023-02-06 15:36:14 +0200

    deleted comments

--- CoffeeBot.py
+++ CoffeeBot.py
@@ -13,6 +13,7 @@ class CoffeeBot_client(discord.Client):
         self.synced = False
         self.added = False
         self.alert_when_online = alert_when_online
+        self.on_ready_callbacks = []
 
     async def on_ready(self):
         await self.wait_until_ready()
@@ -30,91 +31,12 @@ class CoffeeBot_client(discord.Client):
                 channel = self.get_channel(int(server_config.get_specific_announcement_channel(guildID)))
                 await channel.send(f'im active, my name is {self.user}')
             print('\t' + str(guild.name))
+        for callback in self.on_ready_callbacks:
+            await callback()
+
+
+    def add_on_ready_callback(self, callback):
+        self.on_ready_callbacks.append(callback)
+    
     def activate(self): #
         self.run(secret_key)
-
-'''
-@client.tree.command(name = 'choose_creation_channel', description='choose a channel for creationg new voice channels')
-async def choose_creation_channel(interaction: discord.Interaction, channel : discord.TextChannel):
-    try:
-        # get server config
-        this_server_config = server_config(interaction.guild.id)
-        if this_server_config.get_creation_vc_channel() != ' ':
-            await channel_modifier.set_writable(client.get_channel(int(this_server_config.get_creation_vc_channel())))
-        # set announcement channel
-        this_server_config.set_creation_vc_channel(channel.id)
-        await channel_modifier.set_readonly(channel)
-        await interaction.response.send_message(f'\"{channel.name}\" was set as vc creation channel')
-
-    except Exception as e:
-        print(str(e))
-        await interaction.response.send_message('error')
-
-
-@client.tree.command(name = 'choose_static_message', description='choose a static message for vc creation channel')
-async def choose_static_message(interaction: discord.Interaction, message : str):
-    try:
-        # get server config
-        this_server_config = server_config(interaction.guild.id)
-
-        if this_server_config.get_creation_vc_channel() == ' ':
-            await interaction.response.send_message('please set a creation channel first')
-            return
-
-        # set message
-        await interaction.response.send_message(f'\"{message}\" was set as static message')
-
-        # get creation channel
-        creation_channel = client.get_channel(int(this_server_config.get_creation_vc_channel()))
-
-        # embed = discord.Embed(title = 'Instant vc creation', description = message)
-
-        await creation_channel.send(content = message, view = views.InsantButtonView(create_new_channel))
-
-        # add view to embed
-        # view = views.MyView()
-
-        
-        # add buttons to embed
-        # buttons = []
-        # for i in range(1, 11):
-        #     buttons.append(app_commands.ActionRow(app_commands.Button(label = str(i), custom_id = str(i))))
-        # await creation_channel.send('choose a number of users to create a vc for', components = buttons)
-
-
-    except Exception as e:
-        print(str(e))
-        await interaction.response.send_message('error')
-
-
-async def create_new_channel(interaction):
-    print('presenting modal')
-    thisModal = modals.InstantModal(title="Modal via Button")
-    thisModal.set_callback_func(button_pressed)
-    await interaction.response.send_modal(thisModal)
-
-async def button_pressed(interaction):
-    # get chosen values from modal
-    users_amount = interaction.data['components'][0]['components'][0]['value']
-    if users_amount == '':
-        users_amount = None
-    else:
-        users_amount = int(users_amount)
-
-    # get bitrate
-    bitrate = interaction.data['components'][1]['components'][0]['value']
-    if bitrate == '':
-        bitrate = None
-    else:
-        # transform to kb
-        bitrate = int(bitrate) * 1000
-    age_restricted = interaction.data['components'][2]['components'][0]['value'].lower()
-    age_restricted = age_restricted == 'yes' or age_restricted == 'y'
-
-
-    new_channel = await interaction.guild.create_voice_channel(name = f'{interaction.user.name}\'s office',
-    user_limit = users_amount, bitrate = bitrate)
-    new_channel.sync_permissions(True)
-    new_channel.nsfw = age_restricted
-    await interaction.response.send_message(f'created a vc for {users_amount} users')
-'''
