1d5dc7d75c44145d38d85c3967bea2665425985f
CupKido <taler@g.jct.ac.il>
2023-02-06 16:59:06 +0200

    added option for pre entered values

--- modals.py
+++ modals.py
@@ -9,12 +9,24 @@ from discord import ui
 class InstantModal(ui.Modal, title='Questionnaire Response'):
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-    
-    def set_callback_func(self, callback_func):
-        self.callback_func = callback_func
+        
     name = ui.TextInput(label='Name', placeholder='Enter a name for the VC', max_length=20, required=False)
     users_limit = ui.TextInput(label='User Limit', placeholder='Enter a number between 1-99 or leave blank for no limit', max_length=2, required=False)
     bitrate = ui.TextInput(label='Bitrate', placeholder='Enter a number between 8-96 or leave blank for default', max_length=2, required=False)
+    def set_callback_func(self, callback_func):
+        self.callback_func = callback_func
+    
+
+    def set_name(self, name):
+        self.name.value = name
+    def set_users_limit(self, users_limit):
+        if users_limit == None:
+            users_limit = ''
+        self.users_limit.value = users_limit
+    def set_bitrate(self, bitrate):
+        if bitrate == None:
+            bitrate = ''
+        self.bitrate.value = bitrate
 
     async def on_submit(self, interaction: discord.Interaction):
         await self.callback_func(interaction)
