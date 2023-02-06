f1c86b3aff5e523bfcdfb923db81b8b6f45eb29b
CupKido <taler@g.jct.ac.il>
2023-02-06 15:36:41 +0200

    took care of non existing channels

--- channel_modifier.py
+++ channel_modifier.py
@@ -1,12 +1,16 @@
 import discord
 
 async def set_readonly(channel, role = None):
+    if channel is None:
+        return
     if role == None:
         role = channel.guild.default_role
     await channel.edit(sync_permissions=True)
     await channel.set_permissions(channel.guild.default_role, send_messages=False, read_messages=True, create_public_threads=False, create_private_threads=False)
 
 async def remove_readonly(channel, role = None):
+    if channel is None:
+        return
     if role == None:
         role = channel.guild.default_role
     await channel.edit(sync_permissions=True)
