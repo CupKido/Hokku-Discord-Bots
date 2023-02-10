00fbec1360968c56c5bc10fe6fe8d60ebdeff6fb
CupKido <taler@g.jct.ac.il>
2023-02-10 23:00:58 +0200

    another desperate try to fix button

--- ui_components_extension/views.py
+++ ui_components_extension/views.py
@@ -5,15 +5,18 @@ from discord.ext import commands
 from DB_instances.server_config_interface import server_config
 
 class InstantButtonView(View): # Create a class called MyView that subclasses discord.ui.View
-    def __init__(self, callback_func):
+    def __init__(self, bot_client):
         super().__init__()
-        self.callback_func = callback_func
+        self.bot_client = bot_client
         
     @button(label="Edit VC", style=discord.ButtonStyle.primary, emoji="😎") 
     async def button_callback(self, interaction, button):
          # Send a message when the button is clicked
          print("button clicked")
-         await self.callback_func(interaction)
+         await self.bot_client.initialize_buttons()
+
+def get_InstantButtonView(bot_client):
+    return InstantButtonView(bot_client)
 
     
 
