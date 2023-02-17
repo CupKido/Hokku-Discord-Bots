c4565078918d8b3ee2acc7386d07270aaaa8e53b
CupKido <taler@g.jct.ac.il>
2023-02-17 14:38:05 +0200

    main refactor

--- Dynamico.py
+++ Dynamico.py
@@ -1,36 +1,19 @@
-import GenericBot
+from GenericBot import GenericBot_client
 from bot_funcionality_extensions.room_opening import room_opening
 from bot_funcionality_extensions.logger import logger
 from bot_funcionality_extensions.event_logger import event_logger
-
+from funcs_loader import add_functionality
+ 
+# setting path
 
 def main():
     # stat the bot
-    CoffeeBot = GenericBot.GenericBot_client(extract_key(2))
+    CoffeeBot = GenericBot_client(extract_key(2))
     funcs = add_functionality(CoffeeBot, room_opening = room_opening, logger=logger, event_logger=event_logger)
     CoffeeBot.activate()
 
-
-
-def add_functionality(bot, **kwargs):
-    print("==========================================================================================")
-    print("Adding functionality to bot\n")
-    func_classes = {}
-    logger = None
-    if 'logger' in kwargs:
-        print("adding logger functionality")
-        logger = kwargs['logger'](bot)
-        func_classes['logger'] = logger
-        kwargs.pop('logger')
-        print('\n')
-
-    for key, value in kwargs.items():
-        print(f"adding {key} functionality")
-        func_classes[key] = value(bot)
-        print('\n')
-    print("==========================================================================================")
-    return func_classes
 def extract_key(index):
     with open('token.txt', 'r') as f:
         return f.read().split("\n")[index]
+    
 main()
