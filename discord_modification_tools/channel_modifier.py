4e1f73d040f3d1884c0105ac40de157538decbf6
CupKido <taler@g.jct.ac.il>
2023-02-23 19:55:38 +0200

    fixed read only

--- discord_modification_tools/channel_modifier.py
+++ discord_modification_tools/channel_modifier.py
@@ -5,7 +5,7 @@ async def set_readonly(channel, role = None):
         return
     if role == None:
         role = discord.utils.get(channel.guild.roles, name = "@everyone")
-    await channel.set_permissions(role, send_messages=False, read_messages=True, create_public_threads=False, create_private_threads=False)
+    await channel.set_permissions(role, send_messages=False, create_public_threads=False, create_private_threads=False)
 
 async def remove_readonly(channel, role = None):
     if channel is None:
