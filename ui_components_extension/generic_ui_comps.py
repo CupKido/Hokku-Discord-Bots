bf5d4548deed09cee8c8845d026b293231d3ee1b
CupKido <taler@g.jct.ac.il>
2023-02-23 10:31:57 +0200

    added generic selection bar

--- ui_components_extension/generic_ui_comps.py
+++ ui_components_extension/generic_ui_comps.py
@@ -33,6 +33,40 @@ class Generic_Button(Button):
     # def set_view(self, view):
     #     self.view = view
 
+class Generic_Select(ui.Select):
+    def __init__(self, **kwargs):
+        if 'callback' in kwargs:
+            self.func = kwargs['callback']
+            kwargs.pop('callback')
+        if 'placeholder' in kwargs.keys():
+            my_placeholder = kwargs['placeholder']
+        else:
+            my_placeholder = None
+        if 'min_values' in kwargs.keys():
+            my_min_values = kwargs['min_values']
+        else:
+            my_min_values = None
+        if 'max_values' in kwargs.keys():
+            my_max_values = kwargs['max_values']
+        else:
+            my_max_values = None
+        if 'options' in kwargs.keys():
+            my_options = kwargs['options']
+        else:
+            my_options = None
+        self.callback = self.callbacks
+        super().__init__(placeholder=my_placeholder,
+                          min_values=my_min_values,
+                            max_values=my_max_values,
+                              options=my_options)
+    
+    async def callbacks(self, interaction):
+        if self.callback is not None:
+            await self.func(interaction, self, self.view)
+
+    # def set_view(self, view):
+    #     self.view = view
+
 
 class Generic_View(View):
     def __init__(self, timeout=None):
@@ -48,7 +82,22 @@ class Generic_View(View):
         return new_button
 
     
-        
+    def add_generic_select(self, placeholder=None, min_values = 0, max_values = None,
+        options = None, callback=None):
+        if max_values is None:
+            max_values = len(options)
+        # create options
+        options_list = [discord.SelectOption(label=option['label'], description=option['description'], value=option['value']) for option in options]
+        new_select = Generic_Select(placeholder=placeholder,
+                                     min_values = min_values,
+                                       max_values = max_values,
+                                         options = options_list,
+                                           callback=callback)
+        #new_select.set_view(self)
+        self.add_item(new_select)
+
+        return new_select
+
 
 
 
