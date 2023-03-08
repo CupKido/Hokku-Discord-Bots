fe29344c4a32a4d829a0f3b8d2efbd7adb05befc
CupKido <taler@g.jct.ac.il>
2023-03-08 02:24:47 +0200

    fixed id error

--- bot_funcionality_extensions/room_opening/room_opening.py
+++ bot_funcionality_extensions/room_opening/room_opening.py
@@ -340,7 +340,7 @@ class room_opening:
             if after.channel is None:
                 return
             this_server_config = server_config(after.channel.guild.id)
-            if this_server_config.get_param(VC_FOR_VC) is not None and after.channel.id in this_server_config.get_param(VC_FOR_VC):
+            if this_server_config.get_param(VC_FOR_VC) is not None and after.channel in this_server_config.get_param(VC_FOR_VC):
                 await self.create_dynamic_channel(member, after.channel, this_server_config)
 
             # self.log(self.active_channels)
