6772a92a2a12a4b9c23e17da0f96eb4cd86ca1b1
CupKido <taler@g.jct.ac.il>
2023-03-10 03:10:20 +0200

    added basic features to actions tester

--- bot_funcionality_extensions/actions_tester.py
+++ bot_funcionality_extensions/actions_tester.py
@@ -44,4 +44,31 @@ class actions_tester:
             await interaction.channel.send(message)
             await interaction.response.send_message('sent message', ephemeral=True)
 
+        @client.tree.command(name = 'send_to', description='sends a message to a user')
+        @commands.has_permissions(administrator=True)
+        async def send_a_message_to_a_user(interaction: discord.Interaction, user : discord.User, message : str):
+            try:
+                await user.send(message)
+                await interaction.response.send_message('sent message', ephemeral=True)
+            except Exception as e:
+                await interaction.response.send_message('failed to send message, ' + str(e), ephemeral=True)
+                # print exception details
+                
+
+        @client.tree.command(name = 'send_to_by_id', description='sends a message to a user')
+        @commands.has_permissions(administrator=True)
+        async def send_a_message_to_a_user_by_id(interaction: discord.Interaction, user_id : str, message : str):
+            try:
+                user = await self.bot_client.fetch_user(int(user_id))
+                await user.send(message)
+                await interaction.response.send_message('sent message', ephemeral=True)
+            except Exception as e:
+                await interaction.response.send_message('failed to send message, ' + str(e), ephemeral=True)
         
+        @client.tree.command(name = 'get_user_id', description='gets a user id')
+        @commands.has_permissions(administrator=True)
+        async def get_user_id(interaction: discord.Interaction, user : discord.User):
+                await interaction.response.send_message(user.name + ': ' + str(user.id), ephemeral=True)
+                await interaction.response.send_message('sent message', ephemeral=True)
+
+        #TODO: move basic features to a separate feature
