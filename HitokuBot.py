a664c4f683ed8e3cc0e612f282932432aed4f39e
CupKido <taler@g.jct.ac.il>
2023-03-11 14:21:27 +0200

    added hitoku bot for confessions feature

---
+++ HitokuBot.py
@@ -0,0 +1,15 @@
+from GenericBot import GenericBot_client
+from bot_funcionality_extensions.event_logger import event_logger
+from bot_funcionality_extensions.confessions import confessions
+
+from dotenv import dotenv_values
+config = dotenv_values('.env')
+
+def main():
+    # stat the bot
+    HitokuBot_token = config['HITOKUBOT']
+    HitokuBot = GenericBot_client(HitokuBot_token, 'J')
+    HitokuBot.add_features(confessions, event_logger)
+    HitokuBot.activate()
+
+main()
