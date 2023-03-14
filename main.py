332ba53f939304da1559c3a0113cb839bfba69e2
CupKido <taler@g.jct.ac.il>
2023-03-15 00:07:29 +0200

    added more "framework-like" support

---
+++ main.py
@@ -0,0 +1,19 @@
+# import your features here, example:
+# from bot_funcionality_extensions.actions_tester import example_feature_class
+
+###################################
+# fill in the following functions #
+###################################
+
+def get_token():
+    # return your discord bot token
+    pass
+
+def get_db_method():
+    # return 'J' for json or 'M' for mongoDB
+    pass
+
+def add_features(new_generic_bot):
+    # add features to the bot with the following syntax:
+    # new_generic_bot.add_features(example_feature_class, example_feature_class2, ...)
+    pass
