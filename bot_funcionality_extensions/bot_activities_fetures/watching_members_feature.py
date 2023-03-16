c11ae697017999e6c15f910e7b934e8dd43a6f5d
CupKido <taler@g.jct.ac.il>
2023-03-16 16:40:09 +0200

    added watching members feature

---
+++ bot_funcionality_extensions/bot_activities_fetures/watching_members_feature.py
@@ -0,0 +1,21 @@
+from Interfaces.BotFeature import BotFeature
+import discord
+from Interfaces.IGenericBot import IGenericBot
+
+class watching_members_feature(BotFeature):
+    def __init__(self, bot: IGenericBot):
+        super().__init__(bot)
+        self.bot_client.add_on_ready_callback(self.update_presence)
+        self.bot_client.add_on_member_join_callback(self.update_presence_member)
+        self.bot_client.add_on_member_remove_callback(self.update_presence_member)
+
+    async def on_ready(self):
+        await self.update_presence()
+    
+    async def update_presence_member(self, member):
+        await self.update_presence()
+        
+
+    async def update_presence(self):
+        await self.bot_client.change_presence(activity = discord.Activity(type = discord.ActivityType.watching, 
+                                                                          name = f'{len(self.bot_client.get_all_members_list())} Members on {len(self.bot_client.guilds)} Servers'))
