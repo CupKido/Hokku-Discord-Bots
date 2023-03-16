1c4bff4e5b051b72949de60200f92dbfae5418e3
CupKido <taler@g.jct.ac.il>
2023-03-16 16:42:17 +0200

    added watching members feature to bots

--- HitokuBot.py
+++ HitokuBot.py
@@ -1,6 +1,6 @@
 from GenericBot import GenericBot_client
-from bot_funcionality_extensions.event_logger import event_logger
 from bot_funcionality_extensions.confessions import confessions
+from bot_funcionality_extensions.bot_activities_fetures.watching_members_feature import watching_members_feature
 
 from dotenv import dotenv_values
 config = dotenv_values('.env')
@@ -8,7 +8,7 @@ def main():
     # stat the bot
     HitokuBot_token = config['HITOKUBOT_TOKEN']
     HitokuBot = GenericBot_client(HitokuBot_token, 'J')
-    HitokuBot.add_features(confessions)
+    HitokuBot.add_features(confessions, watching_members_feature)
     HitokuBot.activate()
 
 main()
--- RegiBot.py
+++ RegiBot.py
@@ -1,11 +1,13 @@
 from GenericBot import GenericBot_client
 from bot_funcionality_extensions.to_do_list.to_do_list import to_do_list
+from bot_funcionality_extensions.bot_activities_fetures.watching_members_feature import watching_members_feature
+
 from dotenv import dotenv_values
 config = dotenv_values('.env')
 
 def main():
     CoffeeBot = GenericBot_client(config['REGIBOT_TOKEN'], 'J')
-    CoffeeBot.add_features(to_do_list)
+    CoffeeBot.add_features(to_do_list, watching_members_feature)
     CoffeeBot.activate()
 
 
