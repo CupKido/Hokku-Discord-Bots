64301319668cf45e7e45b9ae6002668fc9e0e8e6
CupKido <taler@g.jct.ac.il>
2023-03-23 00:18:33 +0200

    fixed feature

--- bot_funcionality_extensions/bot_activities_fetures/watching_members_feature.py
+++ bot_funcionality_extensions/bot_activities_fetures/watching_members_feature.py
@@ -8,13 +8,15 @@ class watching_members_feature(BotFeature):
         self.bot_client.add_on_ready_callback(self.update_presence)
         self.bot_client.add_on_member_join_callback(self.update_presence_member)
         self.bot_client.add_on_member_remove_callback(self.update_presence_member)
-
+        self.bot_client.add_on_guild_join_callback(self.update_presence_guild)
     async def on_ready(self):
         await self.update_presence()
     
     async def update_presence_member(self, member):
         await self.update_presence()
         
+    async def update_presence_guild(self, guild):
+        await self.update_presence()
 
     async def update_presence(self):
         await self.bot_client.change_presence(activity = discord.Activity(type = discord.ActivityType.watching, 
