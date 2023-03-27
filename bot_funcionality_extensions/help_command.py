3996d9b60ddc34c0b8ab68fcc9cefc36e12f3509
CupKido <taler@g.jct.ac.il>
2023-03-27 22:43:20 +0300

    fixed help_command feature

--- bot_funcionality_extensions/help_command.py
+++ bot_funcionality_extensions/help_command.py
@@ -5,14 +5,13 @@ class help_command(BotFeature):
     def __init__(self, bot):
         super().__init__(bot)
         
-        bot.add_on_ready_callback(self.on_help_command)
-
-    async def on_help_command(self):
-
         @self.bot_client.tree.command(name='help', description='Get info about available commands')
         async def help_command(interaction):
-            message = 'Available commands:\n'
+            await self.on_help_command(interaction)
+
+    async def on_help_command(self, interaction):
+            message = f'Available commands for {interaction.user.mention}:\n'
             for x in self.bot_client.tree.get_commands():
-                if await x._check_can_run(interaction.user):
+                if await x._check_can_run(interaction):
                     message += '\nCommand: /' + x.qualified_name + '\nDescription: ' + x.description + '\n'
             await interaction.response.send_message(message, ephemeral=True)
