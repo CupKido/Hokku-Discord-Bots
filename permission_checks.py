21d8350804731107a52e35597d3ff91797b7f641
CupKido <taler@g.jct.ac.il>
2023-03-28 01:14:48 +0300

    allowed admin commands on DMs

--- permission_checks.py
+++ permission_checks.py
@@ -1,3 +1,6 @@
+import discord
 
 def is_admin(interaction):
+    if type(interaction.channel) is discord.channel.PartialMessageable or type(interaction.channel) is discord.channel.DMChannel:
+        return True
     return interaction.user.guild_permissions.administrator
