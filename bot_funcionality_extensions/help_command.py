ab62dea2c4fd4b8a39543d6d9806ff4f02cb5d4b
CupKido <taler@g.jct.ac.il>
2023-03-28 04:17:11 +0300

    improved "help_commands" by adding pages view

--- bot_funcionality_extensions/help_command.py
+++ bot_funcionality_extensions/help_command.py
@@ -1,6 +1,6 @@
 import discord
 from Interfaces.BotFeature import BotFeature
-
+from ui_components_extension.views.embed_pages import embed_pages
 class help_command(BotFeature):
     def __init__(self, bot):
         super().__init__(bot)
@@ -10,9 +10,23 @@ class help_command(BotFeature):
             await self.on_help_command(interaction)
 
     async def on_help_command(self, interaction):
-            message = f'Available commands for {interaction.user.mention}:\n'
+            embed_title = discord.Embed(title='Help', 
+                                        description='\n**Bot\'s prefix is: \'' + 
+                                        self.bot_client.command_prefix + '\'**' + 
+                                        '\nAvailable commands for ' + interaction.user.mention)
+            owner = self.bot_client.get_user(self.bot_client.owner_id)
+            if owner is None:
+                owner = self.bot_client.get_user(427464593351114754)
+            if owner is not None:
+                embed_title.set_footer(icon_url=owner.avatar.url, text='Bot created by ' + 
+                                       owner.name + '#' + owner.discriminator)
+            else:
+                embed_title.set_footer(icon_url=self.bot_client.user.avatar.url)
+            embeds = []
             for x in self.bot_client.tree.get_commands():
                 if await x._check_can_run(interaction):
-                    message += '\nCommand: /' + x.qualified_name + '\nDescription: ' + x.description + '\n'
-            message += '\nBot\'s prefix is: \'' + self.bot_client.command_prefix + '\''
-            await interaction.response.send_message(message, ephemeral=True)
+                    embed = discord.Embed(title='/' + x.qualified_name, description=x.description, color=0x22ffaa)
+                    embed.set_footer(text='__________________________________________________________________')
+                    embeds.append(embed)
+            pages = embed_pages(embeds=embeds, embed_title=embed_title, items_per_page=5)
+            await pages.send(interaction, ephemeral=True)
