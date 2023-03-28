4613be023ceb664ae2b0f419dbb402c02ed0ebfb
CupKido <taler@g.jct.ac.il>
2023-03-28 03:19:57 +0300

    fixed bugs

--- ui_components_extension/views/embed_pages.py
+++ ui_components_extension/views/embed_pages.py
@@ -1,9 +1,9 @@
 import discord
-
+import math
 class embed_pages(discord.ui.View):
     def __init__(self, embeds, timeout=None, embed_title=None, title='', items_per_page=10):
         super().__init__(timeout=timeout)
-        if embed_pages is None:
+        if items_per_page is None:
             max_items = 10
         else:
             max_items = 9
@@ -17,32 +17,40 @@ class embed_pages(discord.ui.View):
         self.embeds = embeds
         self.current_page = 0
         self.message = None
-        self.add_item(discord.ui.Button(label='Previous', custom_id='previous'))
-        self.add_item(discord.ui.Button(label='Next', custom_id='next'))
+        self.last_page = float(len(self.embeds)) / float(self.items_per_page) - 1
+        self.last_page = int(math.ceil(self.last_page))
+        
     
-    async def send(self, interaction):
+    async def send(self, interaction, ephemeral=False):
         if self.message is None:
-            interaction.response.send_message(content=self.title, embeds=await self.get_current_page_embeds(), view=self)
+            self.message = await interaction.response.send_message(content=self.title, 
+                                                                   embeds=self.get_current_page_embeds(), 
+                                                                   view=self, 
+                                                                   ephemeral=ephemeral)
 
-    async def get_current_page_embeds(self):
+    def get_current_page_embeds(self):
         res = []
         if self.embed_title is not None:
-            res += self.embed_title
+            res.append(self.embed_title)
         for i in range(self.current_page * self.items_per_page, (self.current_page + 1) * self.items_per_page):
             if i >= len(self.embeds):
                 break
-            res += self.embeds[i]
+            res.append(self.embeds[i])
         return res
 
-    @discord.ui.button(custom_id='previous')
-    async def previous(self, button, interaction):
+    @discord.ui.button(label='Previous' ,custom_id='previous')
+    async def previous(self, interaction, button):
         if self.current_page > 0:
             self.current_page -= 1
-            await self.message.edit(embeds=await self.get_current_page_embeds())
+            await interaction.response.edit_message(embeds=self.get_current_page_embeds())
+        else:
+            await interaction.response.defer()
     
-    @discord.ui.button(custom_id='next')
-    async def next(self, button, interaction):
-        if self.current_page < len(self.embeds) // self.items_per_page:
+    @discord.ui.button(label='Next' ,custom_id='next')
+    async def next(self, interaction, button):
+        if self.current_page < self.last_page:
             self.current_page += 1
-            await self.message.edit(embeds=await self.get_current_page_embeds())
+            await interaction.response.edit_message(embeds=self.get_current_page_embeds())
+        else:
+            await interaction.response.defer()
     
