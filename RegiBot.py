a5b3c690110f06298c4e500c7c51e597f391af5a
CupKido <taler@g.jct.ac.il>
2023-03-29 18:20:51 +0300

    added gpt3.5 to RegiBot

--- RegiBot.py
+++ RegiBot.py
@@ -3,13 +3,14 @@ from bot_funcionality_extensions.to_do_list.to_do_list import to_do_list
 from bot_funcionality_extensions.bot_activities_fetures.watching_members_feature import watching_members_feature
 from bot_funcionality_extensions.prefix_adapter import prefix_adapter
 from bot_funcionality_extensions.help_command import help_command
+from bot_funcionality_extensions.OpenAI_features.gpt3_5_api import gpt3_5_api
 
 from dotenv import dotenv_values
 config = dotenv_values('.env')
 
 def main():
     CoffeeBot = GenericBot_client(config['REGIBOT_TOKEN'], 'J', command_prefix='R?')
-    CoffeeBot.add_features(to_do_list, watching_members_feature, help_command, prefix_adapter)
+    CoffeeBot.add_features(to_do_list, watching_members_feature, help_command, gpt3_5_api, prefix_adapter)
     CoffeeBot.activate()
 
 
