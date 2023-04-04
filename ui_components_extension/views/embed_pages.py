3a58df670b61dea72696e998f2fbee8430096ba5
CupKido <taler@g.jct.ac.il>
2023-04-04 22:28:33 +0300

    fixed extra views option

--- ui_components_extension/views/embed_pages.py
+++ ui_components_extension/views/embed_pages.py
@@ -21,21 +21,24 @@ class embed_pages(discord.ui.View):
         self.last_page = int(math.ceil(self.last_page))
         
     
-    async def send(self, interaction, ephemeral=False, followup=False, extra_views=[]):
+    async def send(self, interaction, ephemeral=False, followup=False, views=[]):
         if self.message is None:
             if self.embed_title is not None:
                 title = self.original_title
             else:
                 title = str(self.current_page+1) + '/' + str(self.last_page + 1) + '\n' + self.original_title
+            for view in views:
+                for item in view.children:
+                    self.add_item(item)
             if followup:
                 self.message = await interaction.followup.send(content=title, 
                                                                 embeds=self.get_current_page_embeds(), 
-                                                                views=[self] + extra_views, 
+                                                                view=self, 
                                                                 ephemeral=ephemeral)
             else:
                 self.message = await interaction.response.send_message(content=title, 
                                                                         embeds=self.get_current_page_embeds(), 
-                                                                        views=[self] + extra_views, 
+                                                                        view=self, 
                                                                         ephemeral=ephemeral)
 
     def get_current_page_embeds(self):
