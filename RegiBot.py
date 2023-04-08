c24aabd1b829d0d87e957cc9b347d684beaf5c6e
CupKido <taler@g.jct.ac.il>
2023-04-08 15:46:04 +0300

    updated dev list for Regi

--- RegiBot.py
+++ RegiBot.py
@@ -15,6 +15,7 @@ def main():
     CoffeeBot = GenericBot_client(config['REGIBOT_TOKEN'], 'J', command_prefix='R?')
     dall_e_api.unlimited_users.append(427464593351114754)
     dall_e_api.unlimited_users.append(1091693766294913145)
+    CoffeeBot.developers_list.append(427464593351114754)
     CoffeeBot.add_features(to_do_list, watching_members_feature, help_command, gpt3_5_api, dall_e_api, google_translator, eventsub_feature, prefix_adapter)
     CoffeeBot.activate()
 
