e9be133b5e91a21daefcf8e8e61549b94a25eb30
CupKido <taler@g.jct.ac.il>
2023-06-07 22:35:16 +0300

    added sync options

--- ext/OpenAI_features/gpt_wrapper.py
+++ ext/OpenAI_features/gpt_wrapper.py
@@ -1,6 +1,7 @@
 import enum 
 import json
 import aiohttp
+import requests
 
 class role_options(enum.Enum):
     user = "user"
@@ -32,7 +33,7 @@ class gpt_wrapper:
         return tokens / instance.get_tokens_per_dollar(model)
 
     @classmethod
-    async def get_response_with_history(instance, user_history : list, model : supported_models, api_key, loaded=False):
+    async def get_response_with_history_async(instance, user_history : list, model : supported_models, api_key, loaded=False, callback=None):
         refactored_user_history = [{"role": "system", "content": "You are a helpful assistant."}]
         if loaded:
             for message in user_history:
@@ -63,11 +64,53 @@ class gpt_wrapper:
                     response_json = await response.json()
                     answer = response_json["choices"][0]["message"]["content"]
                     token_amount = response_json['usage']['total_tokens']
-                    return answer, token_amount
+                    if callback is None:
+                        return answer, token_amount
+                    else:
+                        await callback(answer, token_amount)
                 else:
                     raise Exception("Sorry, we have encountered an error. Please try again")
 
     @classmethod
-    async def get_response(instance, message : str, model : supported_models, api_key):
+    async def get_response_async(instance, message : str, model : supported_models, api_key, callback=None):
         user_history = [(role_options.user, message)]
-        return await instance.get_response_with_history(user_history, model, api_key)
+        return await instance.get_response_with_history(user_history, model, api_key, callback=callback)
+    
+    @classmethod
+    def get_response_with_history(instance, user_history : list, model : supported_models, api_key, loaded=False):
+        refactored_user_history = [{"role": "system", "content": "You are a helpful assistant."}]
+        if loaded:
+            for message in user_history:
+                refactored_user_history.append(message)
+        else:
+            for message in user_history:
+                if message[0] == role_options.user:
+                    refactored_user_history.append({"role":"user", "content" : message[1]})
+                elif message[0] == role_options.assistant:
+                    refactored_user_history.append({"role":"assistant", "content" : message[1]})
+
+        chatgpt_url = "https://api.openai.com/v1/chat/completions"
+
+        headers = {
+            "Content-Type": "application/json",
+            "Authorization": "Bearer " + api_key  # Replace with your actual API key
+        }
+        
+        data = {
+            "model" : model.value,
+            #"model" : "gpt-4-0314",
+            "messages": refactored_user_history
+        }
+        response = requests.post(chatgpt_url, headers=headers, data=json.dumps(data))
+        if response.status == 200:
+            response_json = response.json()
+            answer = response_json["choices"][0]["message"]["content"]
+            token_amount = response_json['usage']['total_tokens']
+            return answer, token_amount
+        else:
+            raise Exception("Sorry, we have encountered an error. Please try again")
+        
+    @classmethod
+    def get_response(instance, message : str, model : supported_models, api_key):
+        user_history = [(role_options.user, message)]
+        return instance.get_response_with_history(user_history, model, api_key)
