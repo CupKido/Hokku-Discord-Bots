c697278366f82bcb2cdea046c524bf4c16ac2e9b
CupKido <taler@g.jct.ac.il>
2023-06-14 13:40:56 +0300

    support for adding init after attrs and support for guilds per feature

--- Interfaces/BotFeature.py
+++ Interfaces/BotFeature.py
@@ -1,11 +1,13 @@
 from Interfaces.IGenericBot import IGenericBot
-
+from discord_demi.demi_guild import demi_guild
 # an abstract class that represents a feature of the bot
 class BotFeature:
     LOG_FEATURE_ATTR_NAME = 'log_feature'
     GUILDS_ATTR_NAME = "guilds"
+    
     def __init__(self, bot : IGenericBot, log_feature=False, guilds=None):
         self.bot_client = bot
+        self.command_inits = []
         self.attrs = {self.LOG_FEATURE_ATTR_NAME : log_feature, self.GUILDS_ATTR_NAME : guilds }
 
     def set_attrs(self, attrs : dict):
@@ -27,7 +29,20 @@ class BotFeature:
 
 
     # TODO: create wrapper that uses guilds in every command 
-    def feature_command(**kwargs):
-        def wrapper(coro):
-            pass 
-        
+
+    def feature_command(self, **kwargs):
+        def deco(coro):
+            def init_command():
+                if self.GUILDS_ATTR_NAME not in kwargs and self.GUILDS_ATTR_NAME in self.attrs.keys() and self.attrs[self.GUILDS_ATTR_NAME] is not None:
+                    new_guilds = []
+                    for guild in self.attrs[self.GUILDS_ATTR_NAME]:
+                        new_guilds.append(demi_guild(guild))
+                    kwargs['guilds'] = new_guilds
+                self.bot_client.generic_command(**kwargs)(coro)
+            self.command_inits.append(init_command)
+        return deco
+
+    def init_commands(self):
+        for init_command in self.command_inits:
+            init_command()
+        self.command_inits = []
---
+++ discord_demi/demi_guild.py
@@ -0,0 +1,3 @@
+class demi_guild:
+    def __init__(self, guild_id):
+        self.id = guild_id
